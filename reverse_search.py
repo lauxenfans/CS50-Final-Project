5fd237649d92bd92535c682919aef256baabc4c5
lauxenfans <lauxenfans@gmail.com>
2023-12-05 15:45:49 -0500

    Ready to generalize reverse search, need Flask,HTML

--- reverse_search.py
+++ reverse_search.py
@@ -1,14 +1,24 @@
 
-from serpapi import GoogleSearch
+
 import requests 
 from PIL import Image 
 from io import BytesIO
+from serpapi import GoogleSearch
 
 ## Need to be able to customize JPG file url 
 
+# User upload the photo - Insert into HTML 
+# request.form.file — Get the file information and save using os.paht.join and display image by finding from the path 
+#SQL has binary blob type and can insert into a blob format 
+# Store as binary large ojbect and find most recent saved image 
+# os.path — Navigate exactly where you want to go 
+
+
+uploaded_image_url = "https://purepng.com/public/uploads/large/purepng.com-applefoodsweettastyhealthyfruitapple-9815246780899e3jo.png"
+
 params = {
   "engine": "google_reverse_image",
-  "image_url": "https://purepng.com/public/uploads/large/purepng.com-applefoodsweettastyhealthyfruitapple-9815246780899e3jo.png",
+  "image_url": uploaded_image_url,
   "api_key": "b6da2d59ed058bbd9aed17229429a0e9a9fd3287ace73d54373536b5d8f4de61"
 }
 

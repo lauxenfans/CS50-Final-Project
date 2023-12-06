ef7ddbf21728e355b65d820d4a81a9bc587d07f4
catherinebli <144501001+catherinebli@users.noreply.github.com>
2023-12-05 23:42:48 -0500

    Update app.py
    new chagnes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sqlite3
+import tkinter as tk #s
 
 from cs50 import SQL
 
@@ -14,8 +15,6 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
-db = SQL("sqlite:///finance.db")
 
 @app.after_request
 def after_request(response):
